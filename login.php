34f071f59a62317cdb101aaa395fe04baf4d399d
Daniel Frycowski <frycowskidan@gmail.com>
2021-03-10 19:59:23 +0000

    Added ability to create login sessions

--- login.php
+++ login.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
-if ((isset($_SESSION['loggedin'])) && ($_SESSION['loggedin']==true))
-{
-    header('Location: sklep.php');
-    exit();
-}
+//if ((isset($_SESSION['loggedin'])) && ($_SESSION['loggedin']==true))
+//{
+//    header('Location: login.php');
+//    exit();
+//}
 ?>
 <!DOCTYPE html>
 <html>
