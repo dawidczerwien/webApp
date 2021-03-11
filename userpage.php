ecc89e9872b90f600468c81e28b1ccee176071a9
Daniel Dzordz <revoo06@gmail.com>
2021-03-11 10:47:17 +0100

    Session fix

--- userpage.php
+++ userpage.php
@@ -3,5 +3,4 @@ session_start();
 echo "Logged in as ".$_SESSION['ulogin']."<br><br>";
 echo "Hi ".$_SESSION['realname'].$_SESSION['realsurname']."<br>";
 echo "Your account's balance is".$_SESSION['ubank']."<br>";
-
 ?>
