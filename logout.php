fd8cf9b6171768b621dfa2685870ff0118350861
Dzordzo <frycowskidan@gmail.com>
2021-04-18 11:48:02 +0200

    next session fix

--- logout.php
+++ logout.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 if ((isset($_SESSION['loggedin'])) && ($_SESSION['loggedin']==true)) {
+    //$_SESSION = array();
+    session_destroy();
     session_write_close();
     header('Location: index.php');
     exit();
