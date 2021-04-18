182d673436c070ae057a5f1f4e97e6db640dd93c
Dzordzo <frycowskidan@gmail.com>
2021-04-18 11:43:19 +0200

    logout fix

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if ((isset($_SESSION['loggedin'])) && ($_SESSION['loggedin']==true)) {
-    session_abort();
+    session_write_close();
     header('Location: index.php');
     exit();
 }
