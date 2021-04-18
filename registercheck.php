2e92114d9960da6f27e323ea8c531b92e477cc1b
Dzordzo <frycowskidan@gmail.com>
2021-04-18 10:36:39 +0200

    added password hashing, password checking

--- registercheck.php
+++ registercheck.php
@@ -8,23 +8,26 @@ try
     $stmt = $conn->prepare('SELECT * FROM users WHERE uname = ? OR uemail = ?');
     $stmt->execute(array($_POST['inlogin'], $_POST['inEmail']));
     $outcome = $stmt->fetch();
-    if($stmt->rowCount() == 0)
-    {
-        $stmt = $conn->prepare("INSERT INTO users (`uname`, `upass`, `uemail`) VALUES (?, ?, ?)");
-        $stmt->execute(array($_POST['inlogin'], $_POST['inpass'], $_POST['inEmail']));
-        unset($_SESSION['err']);
-		header('Location: login.php');
-		exit();
-    }
-	else 
-	{
+    if($stmt->rowCount() == 0) {
+        if(inpass == inConfirmPass) {
+            $passhash = password_hash($_POST['inpass'], PASSWORD_BCRYPT);
+            $stmt = $conn->prepare("INSERT INTO users (`uname`, `upass`, `uemail`) VALUES (?, ?, ?)");
+            $stmt->execute(array($_POST['inlogin'], $passhash, $_POST['inEmail']));
+            unset($_SESSION['err']);
+            header('Location: login.php');
+            exit();
+        } else {
+            $_SESSION['err'] = '<span style="color:red">Passwords are not the same!</span>';
+            header('Location: register.php');
+            exit();
+        }
+    } else {
 		$_SESSION['err'] = '<span style="color:red">User with such login or email already exists!</span>';
 		header('Location: register.php');
 		exit();
 	}
 }
-catch(PDOException $e)
-{
+catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
 ?>
