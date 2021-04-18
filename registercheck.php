4c664443d0e5d8cf9e303463e7f2300187c34e57
Dzordzo <frycowskidan@gmail.com>
2021-04-18 12:17:46 +0200

    added password checking

--- registercheck.php
+++ registercheck.php
@@ -9,12 +9,23 @@ try {
     $outcome = $stmt->fetch();
     if($stmt->rowCount() == 0) {
         if($_POST['inpass'] == $_POST['inConfirmPass']) {
-            $passhash = password_hash($_POST['inpass'], PASSWORD_BCRYPT);
-            $stmt = $conn->prepare("INSERT INTO users (`uname`, `upass`, `uemail`) VALUES (?, ?, ?)");
-            $stmt->execute(array($_POST['inlogin'], $passhash, $_POST['inEmail']));
-            unset($_SESSION['err']);
-            header('Location: login.php');
-            exit();
+            $passTMP = $_POST['inpass'];
+            $hasUppercase = preg_match('@[A-Z]@', $passTMP);
+            $hasLowercase = preg_match('@[a-z]@', $passTMP);
+            $hasNumber    = preg_match('@[0-9]@', $passTMP);
+            $hasSpecialChars = preg_match('@[^\w]@', $passTMP);
+            if(!$hasUppercase || !$hasLowercase || !$hasNumber || !$hasSpecialChars || strlen($passTMP) < 8) {
+                $_SESSION['err'] = '<span style="color:red">Password should be at least 8 characters in length and should include at least one upper case letter, one number, and one special character!</span>';
+                header('Location: register.php');
+                exit();
+            } else {
+                $passhash = password_hash($_POST['inpass'], PASSWORD_BCRYPT);
+                $stmt = $conn->prepare("INSERT INTO users (`uname`, `upass`, `uemail`) VALUES (?, ?, ?)");
+                $stmt->execute(array($_POST['inlogin'], $passhash, $_POST['inEmail']));
+                unset($_SESSION['err']);
+                header('Location: login.php');
+                exit();
+            }
         } else {
             $_SESSION['err'] = '<span style="color:red">Passwords are not the same!</span>';
             header('Location: register.php');
