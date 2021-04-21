c9a9bee0992ba8acfdafa09b1a2d5b4db1c8fcd6
Dzordzo <frycowskidan@gmail.com>
2021-04-21 22:21:59 +0200

    fix

--- userpage.php
+++ userpage.php
@@ -4,7 +4,7 @@ if ((!isset($_SESSION['loggedin'])) && ($_SESSION['loggedin']==false)) {
     header('Location: login.php');
     exit();
 }
-echo "Logged in as ".$_SESSION['uname']."<br><br>";
+echo "<h2>Logged in as ".$_SESSION['uname']."<h2><br><br>";
 echo "Hi ".$_SESSION['realname'].$_SESSION['realsurname']."<br>";
 echo "Your account's balance is: ".$_SESSION['ubank']."<br>";
 ?>
