0994dc94846d4ae48b6fb9e7a2063f178a3cec18
Dzordzo <frycowskidan@gmail.com>
2021-04-22 10:15:58 +0200

    test

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
             <?php
             session_start();
             if ((isset($_SESSION['loggedin'])) && ($_SESSION['loggedin']==true)) {
-                echo '<p>Logged in as Aaaaa ?:  ';
+                echo '<p>Logged in as 1234 ?:  ';
                 echo $_SESSION['uname'];
                 echo '</p><br>';
                 echo '<a href="logout.php"><p>LOGOUT</p></a>';
