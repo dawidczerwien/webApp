21f2701a95b657b23881c80b5f8b186b5671d3ed
Daniel Frycowski <74418919+Frydan@users.noreply.github.com>
2021-06-09 19:34:18 +0200

    visual update cart

--- usercart.php
+++ usercart.php
@@ -18,14 +18,13 @@ try {
     if($stmt->rowCount() > 0) {
         echo "Your cart items: <br><br>";
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            echo "prodID:".$row['prodid']."<br>";
             $stmt2 = $conn->prepare('SELECT * FROM prod WHERE id = ?');
             $stmt2->execute(array($row['prodid']));
             if($stmt2->rowCount() > 0) {
                 while ($row2 = $stmt2->fetch(PDO::FETCH_ASSOC)) {
-                    echo "id:".$row2['id']."<br>";
-                    echo "price per item:".$row2['price']."<br>";
-                    echo "name:".$row2['name']."<br>";
+                    echo "Product's ID: ".$row2['id']."<br>";
+                    echo "Price per item: ".$row2['price']."ZŁ<br>";
+                    echo "Product's Name: ".$row2['name']."<br>";
                     echo "<hr>";
                 }
             }
