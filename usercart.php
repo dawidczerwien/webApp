c701364da493de40529a07a64d5217496fa7fd1f
Daniel Frycowski <74418919+Frydan@users.noreply.github.com>
2021-06-09 19:31:26 +0200

    Cart update

--- usercart.php
+++ usercart.php
@@ -10,7 +10,7 @@ try {
     $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     if (isset($_SESSION['mess'])){
-        echo $_SESSION['mess'];
+        echo $_SESSION['mess']."<br><br>";
         unset($_SESSION['mess']);
     }
     $stmt = $conn->prepare('SELECT * FROM cart WHERE userid = ?');
@@ -18,7 +18,16 @@ try {
     if($stmt->rowCount() > 0) {
         echo "Your cart items: <br><br>";
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            echo "ID:".$row['prodid']."<br>";
+            echo "prodID:".$row['prodid']."<br>";
+            $stmt2 = $conn->prepare('SELECT * FROM prod WHERE id = ?');
+            $stmt2->execute(array($row['prodid']));
+            if($stmt2->rowCount() > 0) {
+                while ($row2 = $stmt2->fetch(PDO::FETCH_ASSOC)) {
+                    echo "id:".$row2['id']."<br>";
+                    echo "price per item:".$row2['price']."<br>";
+                    echo "name:".$row2['name']."<br>";
+                }
+            }
         }
     }
 }
