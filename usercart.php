6e68b9f9e7aa06aa36f2a19f4faa54d7f67b756b
Daniel Frycowski <74418919+Frydan@users.noreply.github.com>
2021-06-09 19:20:17 +0200

    cart fixes

--- usercart.php
+++ usercart.php
@@ -16,7 +16,7 @@ try {
     $stmt = $conn->prepare('SELECT * FROM cart WHERE userid = ?');
     $stmt->execute(array($_SESSION['id']));
     if($stmt->rowCount() > 0) {
-        echo "Your cart items: <br><br>"
+        echo "Your cart items: <br><br>";
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             echo "ID:".$row['prodid']."<br>";
         }
