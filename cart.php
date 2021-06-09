b4e12f4fc3c79f126c693090a131ee25bd441811
Daniel Frycowski <74418919+Frydan@users.noreply.github.com>
2021-06-09 18:58:13 +0200

    added cart message

--- cart.php
+++ cart.php
@@ -8,6 +8,7 @@ if ((!isset($_SESSION['loggedin'])) && ($_SESSION['loggedin']==false)) {
 function addProduct($conn, $UID, $PID) {
     $stmt = $conn->prepare("INSERT INTO cart (`userid`, `prodid`) VALUES (?, ?)");
     $stmt->execute(array($UID, $PID));
+    return "Product added to Your cart!";
 }
 
 
@@ -16,7 +17,8 @@ echo "USER's PID:".$_POST['PID']."<br><br>";
 try {
     $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    addProduct($conn, $_SESSION['id'], $_POST['PID']);
+    $resp = addProduct($conn, $_SESSION['id'], $_POST['PID']);
+    echo $resp;
     $stmt = $conn->prepare('SELECT * FROM cart WHERE userid = ?');
     $stmt->execute(array($_SESSION['id']));
     if($stmt->rowCount() > 0) {
