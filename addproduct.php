c8f99acee7ca1bf45691c70f8e564afd93c0d86c
dawidCze <dawidcze8@gmail.com>
2021-06-16 21:08:05 +0200

    new styles

--- addproduct.php
+++ addproduct.php
@@ -30,7 +30,7 @@ if ((!isset($_SESSION['loggedin'])) && ($_SESSION['loggedin']==false)) {
         ?>
         <button type="submit">Add new!</button>
     </form>
-    <div class="container">
+    <div class="go_back_container">
     <form class="text" action="userpage.php" method="POST">
         <button type="submit">Back to User Page</button>
     </form>
